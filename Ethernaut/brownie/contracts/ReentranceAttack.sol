71538f8d077ee548d7547ac808099c7cb9910b8d
y-chi <yannick.chi@gmail.com>
2022-08-07 21:00:02 +0200

    Fix Reentrance

--- Ethernaut/brownie/contracts/ReentranceAttack.sol
+++ Ethernaut/brownie/contracts/ReentranceAttack.sol
@@ -1,7 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+import "@openzeppelin/contracts/math/SafeMath.sol";
+
+contract Reentrance {
+    using SafeMath for uint256;
+    mapping(address => uint256) public balances;
+
+    function donate(address _to) public payable {
+        balances[_to] = balances[_to].add(msg.value);
+    }
+
+    function balanceOf(address _who) public view returns (uint256 balance) {
+        return balances[_who];
+    }
+
+    function withdraw(uint256 _amount) public {
+        if (balances[msg.sender] >= _amount) {
+            (bool result, ) = msg.sender.call{value: _amount}("");
+            if (result) {
+                _amount;
+            }
+            balances[msg.sender] -= _amount;
+        }
+    }
+
+    receive() external payable {}
+}
+
 contract ReentranceAttack {
     Reentrance reentrance;
 
-    constructor(address payable _reentrance) payable {
+    constructor(address payable _reentrance) public payable {
         reentrance = Reentrance(_reentrance);
     }
 
